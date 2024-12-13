fd857467a51b487fa3a41b335f9787bc0586118e
k1no <veredyukov30@gmail.com>
2024-12-13 14:10:48 +0300

    задача

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const positionLatitude = 100
+    , positionLongitude = 50
+    , destinationLatitude = 100
+    , destinationLongitude = 50;
+
+console.log(`Рашатеся все через формулу d = v (x A - x B) 2 + (y A - y B) 2, таким образом подставялем в формулу - d = Math.sqrt((positionLatitude - positionLongitude)**2 + (destinationLatitude - destinationLongitude)**2) и получаем ответ: ${d = Math.sqrt((positionLatitude - positionLongitude)**2 + (destinationLatitude - destinationLongitude)**2)}`);
